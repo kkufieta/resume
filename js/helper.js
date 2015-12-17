7e6041bf865652057a6c9426daa1de7c8a479ff8
Katharina Xenia Kufieta <katharina.kufieta@gmail.com>
2015-12-17 15:25:57 +0100

    Add google map which shows job locations

--- js/helper.js
+++ js/helper.js
@@ -160,9 +160,10 @@ function initializeMap() {
       content: name
     });
 
-    // hmmmm, I wonder what this is about...
     google.maps.event.addListener(marker, 'click', function() {
-      // your code goes here!
+      // creates an infoWindow objec that determines the properties
+      // of the overlay
+      infoWindow.open(map, marker);
     });
 
     // this is where the pin actually gets added to the map.
@@ -225,11 +226,11 @@ Uncomment the code below when you're ready to implement a Google Map!
 */
 
 // Calls the initializeMap() function when the page loads
-//window.addEventListener('load', initializeMap);
+window.addEventListener('load', initializeMap);
 
 // Vanilla JS way to listen for resizing of the window
 // and adjust map bounds
-//window.addEventListener('resize', function(e) {
-  //Make sure the map bounds get updated on page resize
-//  map.fitBounds(mapBounds);
-//});
+window.addEventListener('resize', function(e) {
+  // Make sure the map bounds get updated on page resize
+  map.fitBounds(mapBounds);
+});
